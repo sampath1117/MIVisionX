dba3897d88534fc66f03391a4ff4664c0e48000f
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2022-02-25 10:58:45 +0000

    reverted files changed due to merge

--- rocAL/rocAL/include/tf_meta_data_reader_detection.h
+++ rocAL/rocAL/include/tf_meta_data_reader_detection.h
@@ -52,8 +52,8 @@ private:
     void add(std::string image_name, BoundingBoxCords bbox, BoundingBoxLabels b_labels, ImgSize image_size);
     bool _last_rec;
     //std::shared_ptr<TF_Read> _TF_read = nullptr;
-    void read_record(std::ifstream &file_contents, uint file_size, std::vector<std::string> &image_name,
-        std::string user_label_key, std::string user_text_key,
+    void read_record(std::ifstream &file_contents, uint file_size, std::vector<std::string> &image_name, 
+        std::string user_label_key, std::string user_text_key, 
         std::string user_xmin_key, std::string user_ymin_key, std::string user_xmax_key, std::string user_ymax_key,
         std::string user_filename_key);    // std::map<std::string, std::shared_ptr<Label>> _map_content;
     // std::map<std::string, std::shared_ptr<Label>>::iterator _itr;
