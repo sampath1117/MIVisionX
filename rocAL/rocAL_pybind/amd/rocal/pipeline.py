5b147f3679362e493c30055609836131d1a57676
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2023-07-06 11:05:16 +0000

    minor code cleanup

--- rocAL/rocAL_pybind/amd/rocal/pipeline.py
+++ rocAL/rocAL_pybind/amd/rocal/pipeline.py
@@ -80,7 +80,6 @@ class Pipeline(object):
                  rocal_cpu=False, max_streams=-1, default_cuda_stream_priority=0,
                  tensor_layout = types.NCHW, reverse_channels = False, multiplier = [1.0,1.0,1.0],
                  offset = [0.0, 0.0, 0.0], tensor_dtype=types.FLOAT, last_batch_policy=types.LAST_BATCH_FILL, last_batch_padded=False):
-        print("in Pipeline.py last_batch_policy", last_batch_policy)
         if(rocal_cpu):
             self._handle = b.rocalCreate(
                 batch_size, types.CPU, device_id, num_threads, prefetch_queue_depth, types.FLOAT, last_batch_policy, last_batch_padded)
